0bc603eaf5db11c0300f731271d34d025a7288d2
Nitin Mogalapalli <mogalapallinitin+github@gmail.com>
2025-10-22 13:05:47 +0530

    first version

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,44 @@
+import { expo } from '@better-auth/expo';
+import { betterAuth, type BetterAuthOptions } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { polar, checkout, portal } from "@polar-sh/better-auth";
+import { polarClient } from "./lib/payments";
+import prisma from "@my-better-t-app/db";
+
+export const auth = betterAuth<BetterAuthOptions>({
+	database: prismaAdapter(prisma, {
+		provider: "postgresql",
+	}),
+	trustedOrigins: [process.env.CORS_ORIGIN || "", "mybettertapp://", "exp://"],
+	emailAndPassword: {
+		enabled: true,
+	},
+	advanced: {
+		defaultCookieAttributes: {
+			sameSite: "none",
+			secure: true,
+			httpOnly: true,
+		},
+	},
+	plugins: [
+		polar({
+			client: polarClient,
+			createCustomerOnSignUp: true,
+			enableCustomerPortal: true,
+			use: [
+				checkout({
+					products: [
+						{
+							productId: "your-product-id",
+							slug: "pro",
+						},
+					],
+					successUrl: process.env.POLAR_SUCCESS_URL,
+					authenticatedUsersOnly: true,
+				}),
+				portal(),
+			],
+		}),
+    expo()
+  ],
+});
